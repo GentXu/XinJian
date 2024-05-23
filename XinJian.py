134f5febe09ad5bc69ff6f8d45755e4a43d47c2f
NuStar <3318029085@qq.com>
2024-05-23 22:30:39 +0800

    更换数据库，重写登录注册逻辑

--- XinJian.py
+++ XinJian.py
@@ -1,20 +1,18 @@
 import threading
 from tkinter import messagebox
 import SocketService
-import MyAuth
 from entity.User import *
 from gui.UserGui import *
+from data.DataBase import *
 
 
-myAuth = MyAuth.MyAuth()
 token = None
+user_data = None
 
 
 class MainGui(Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        user_result = myAuth.get_user_info("ndx123")
-        self.user = User(user_result["result"]["name"], user_result["result"]["remark"], udp_cli.get_ip(), token)
         self.master = master
         self.master.resizable(0, 0)
         self.pack()
@@ -28,31 +26,44 @@ class MainGui(Frame):
         self.master.config(menu=self.menu_bar)
 
     def user_gui(self):
-        UserGui(self.user, self.master)
+        UserGui(user_data, self.master)
 
 
 class Register:
     def __init__(self, master):
         self.master = master
+        # 窗口始终在master的上方
         top = self.top = tkinter.Toplevel(master)
         top.title("[信笺]用户注册")
-        top.geometry("200x300+900+400")
+        top.geometry("200x350+900+400")
+        # 设为root的临时子窗口
         top.transient(root)
+        # 设置windows关闭协议触发on_close方法
         top.protocol("WM_DELETE_WINDOW", self.on_close)
+        # 文本框变量
         user = StringVar()
         password = StringVar()
         name = StringVar()
-        remark = StringVar()
+        motto = StringVar()
+        birthday = StringVar()
+        self.xb = BooleanVar()
+        """
+        初始化控件
+        """
         self.user_entry = Entry(top, textvariable=user, width=15)
         self.user_label = Label(top, text="账号：")
         self.password_label = Label(top, text="密码：")
         self.password_entry = Entry(top, textvariable=password, show="*", width=15)
         self.name_label = Label(top, text="昵称：")
         self.name_entry = Entry(top, textvariable=name, width=15)
-        self.remark_label = Label(top, text="备注：")
-        self.remark_entry = Entry(top, textvariable=remark, width=15)
+        self.motto_label = Label(top, text="备注：")
+        self.motto_entry = Entry(top, textvariable=motto, width=15)
+        self.birthday_label = Label(top, text="生日：")
+        self.birthday_entry = Entry(top, textvariable=birthday, width=15)
+        self.xb_label = Label(top, text="性别:")
+        self.xb_radiobutton = Radiobutton(top, text="男", variable=self.xb, value=1)
+        self.xb_radiobutton2 = Radiobutton(top, text="女", variable=self.xb, value=0)
         self.register_button = Button(top, text="注册", command=self.register, width=10)
-        self.master = master
         self.create_widgets()
 
     def create_widgets(self):
@@ -62,18 +73,23 @@ class Register:
         self.password_label.place(relx=0.05, rely=0.25)
         self.name_entry.place(relx=0.25, rely=0.4)
         self.name_label.place(relx=0.05, rely=0.4)
-        self.remark_entry.place(relx=0.25, rely=0.55)
-        self.remark_label.place(relx=0.05, rely=0.55)
-        self.register_button.place(relx=0.3, rely=0.7)
+        self.motto_entry.place(relx=0.25, rely=0.55)
+        self.motto_label.place(relx=0.05, rely=0.55)
+        self.birthday_label.place(relx=0.05, rely=0.7)
+        self.birthday_entry.place(relx=0.25, rely=0.7)
+        self.xb_label.place(relx=0.05, rely=0.80)
+        self.xb_radiobutton.place(relx=0.25, rely=0.80)
+        self.xb_radiobutton2.place(relx=0.45, rely=0.80)
+        self.register_button.place(relx=0.3, rely=0.9)
 
     def register(self):
-        result = myAuth.register(self.user_entry.get(), self.password_entry.get(),
-                                 self.name_entry.get(), self.remark_entry.get())
-        code = result["code"]
-        msg = result["msg"]
-        messagebox.showinfo(code, msg)
-        if code == 200:
-            self.on_close()
+        result = data_base.register(user=self.user_entry.get(), name=self.name_entry.get(),
+                                    passwd=self.password_entry.get(), xb=self.xb.get(),
+                                    birthday=self.birthday_entry.get(), motto=self.motto_entry.get())
+        if result["msg"]:
+            messagebox.showinfo("提示", "注册成功！")
+        else:
+            messagebox.showerror("提示", "注册失败！联系管理员！")
 
     def on_close(self):
         self.master.login_button.config(state="normal")
@@ -83,25 +99,29 @@ class Register:
         self.top.destroy()
 
 
-class Application(Frame):
+class Login(Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        user = StringVar()
-        passwd = StringVar()
-        self.user_entry = Entry(self, textvariable=user)
-        self.user_label = Label(self, text="账号：")
-        self.password_label = Label(self, text="密码：")
-        self.password_entry = Entry(self, textvariable=passwd, show="*")
-        self.login_button = Button(self, text="登录", command=self.login, width=10)
-        self.register_button = Button(self, text="注册", command=self.register, width=10)
         self.master = master
         self["width"] = 300
         self["height"] = 200
         self.pack()
+        # 账号密码框的变量
+        user = StringVar()
+        self.passwd = StringVar()
+        """
+        初始化控件
+        """
+        self.user_entry = Entry(self, textvariable=user)
+        self.user_label = Label(self, text="账号：")
+        self.password_label = Label(self, text="密码：")
+        self.password_entry = Entry(self, textvariable=self.passwd, show="*")
+        self.login_button = Button(self, text="登录", command=self.login, width=10)
+        self.register_button = Button(self, text="注册", command=self.register, width=10)
         self.create_widgets()
 
+    """创建组件"""
     def create_widgets(self):
-        """创建组件"""
         self.user_label.place(relx=0.1, rely=0.2)
         self.user_entry.place(relx=0.3, rely=0.2)
         self.password_label.place(relx=0.1, rely=0.4)
@@ -113,15 +133,18 @@ class Application(Frame):
         global token
         user = self.user_entry.get()
         password = self.password_entry.get()
-        result = myAuth.login(user, password)
-        code = result["code"]
-        msg = result["msg"]
-        if code == 200:
+        result = data_base.login(user=user, passwd=password)
+        if result["msg"]:
+            # token = result["result"]["token"]
+            # threading.Timer(200, myAuth.heartbeat, args=(token,))
+            global user_data
+            # 解包字典 实例化User
+            user_data = User(**data_base.get_user_info(user=user))
+            print(user_data.get_user())
+
             """
-            销毁登录界面，新建一个父窗口为用户界面
+            销毁登录界面，新建一个父窗口
             """
-            token = result["result"]["token"]
-            threading.Timer(200, myAuth.heartbeat, args=(token,))
             self.master.destroy()
             main_gui = Tk()
             main_gui.title("XinJian")
@@ -129,7 +152,8 @@ class Application(Frame):
             MainGui(main_gui)
             main_gui.mainloop()
         else:
-            messagebox.showinfo(code, msg)
+            messagebox.showerror("提示", "密码错误！请重新输入")
+            self.passwd.set("")
 
     def register(self, **kwargs):
         Register(self)
@@ -140,9 +164,14 @@ class Application(Frame):
 
 
 if __name__ == '__main__':
-    udp_cli = SocketService.SocketLoad()
+    # 初始化全局协议
+    udp_server = SocketService.UDPSocketLoad()
+    tcp_server = SocketService.TCPSocketLoad()
+    # 初始化数据库
+    data_base = DataBase(tcp_server)
+    # 登录界面
     root = Tk()
     root.title("[信笺]登录界面")
     root.geometry("300x200+700+400")
-    Application(master=root)
+    Login(master=root)
     root.mainloop()
