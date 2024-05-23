4d2226d102b315c7ac2c38143d176f56d6f4885e
NuStar <3318029085@qq.com>
2024-05-23 22:29:34 +0800

    增加TCP连接

--- SocketService.py
+++ SocketService.py
@@ -1,22 +1,30 @@
+import json
 import socket
 import threading
 
 
-class SocketLoad:
+class UDPSocketLoad:
     def __init__(self):
         self.host = '222.187.254.111'
         self.port = 15061
         self.udp_cli = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.ip = None
         self.initialize()
+        # 开一个线程接受服务端消息
         threading.Thread(target=self.recv_msg).start()
 
     def get_ip(self):
         return self.ip
 
+    """
+    向服务端发送消息
+    """
     def send_msg(self, msg):
         self.udp_cli.sendto(msg.encode('gbk'), (self.host, self.port))
 
+    """
+    处理服务端的消息
+    """
     def recv_msg(self):
         while True:
             data, addr = self.udp_cli.recvfrom(1024)
@@ -34,3 +42,29 @@ class SocketLoad:
 
     def initialize(self):
         self.send_msg("Client@initialize")
+
+
+class TCPSocketLoad:
+    def __init__(self):
+        self.host = '222.187.254.111'
+        self.port = 15061
+        self.tcp_cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_cli.connect((self.host, self.port))
+
+    """
+    服务端消息处理
+    """
+    def recv_msg(self):
+        data = self.tcp_cli.recv(1024).decode('utf-8')
+        dict_data = json.loads(data)
+        return dict_data
+
+    """
+    向服务端发送消息
+    """
+    def send_msg(self, msg):
+        self.tcp_cli.sendall(msg.encode('utf-8'))
+
+
+if __name__ == '__main__':
+    tcp = TCPSocketLoad()
